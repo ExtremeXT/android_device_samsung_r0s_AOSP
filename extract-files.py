1da9755966131f7f6e1889c3e9451f7c8062eec6
ExtremeXT <extremextdev@gmail.com>
2025-10-22 22:01:37 +0300

    remove 32bit libs from extract, add camera hexpatches
    Change-Id: Iba15eee3872be24e7178f390c690cc00d970e961
    Signed-off-by: ExtremeXT <extremextdev@gmail.com>

--- extract-files.py
+++ extract-files.py
@@ -48,7 +48,6 @@ blob_fixups: blob_fixups_user_type = {
     'vendor/lib64/hw/camera.s5e9925.so':blob_fixup()
         .add_needed('libshim_ui.so'),
     (
-        'vendor/lib/hw/vulkan.samsung.so',
         'vendor/lib64/hw/vulkan.samsung.so',
     ): blob_fixup()
         .clear_symbol_version('AHardwareBuffer_acquire')
@@ -58,7 +57,6 @@ blob_fixups: blob_fixups_user_type = {
         .clear_symbol_version('AHardwareBuffer_getNativeHandle')
         .clear_symbol_version('AHardwareBuffer_release'),
     (
-        'vendor/lib/libOpenCL.so',
         'vendor/lib64/libOpenCL.so',
     ): blob_fixup()
         .clear_symbol_version('AHardwareBuffer_acquire')
@@ -105,15 +103,15 @@ blob_fixups: blob_fixups_user_type = {
             'android.hardware.security.keymint-V1-ndk.so')
         .add_needed('android.hardware.security.rkp-V3-ndk.so'),
     (
-        'vendor/lib/libexynoscamera3.so',
         'vendor/lib64/libexynoscamera3.so',
     ): blob_fixup()
+        .sig_replace('88 02 00 36 21 0F 00 D0', '1F 20 03 D5 21 0F 00 D0')
+        .sig_replace('16 00 00 94 0B 00 00 14', '1F 20 03 D5 0B 00 00 14')
+        .sig_replace('92 FF FF 97 0B 00 00 14', '1F 20 03 D5 0B 00 00 14')
         .add_needed('libshim_camera.so')
         .add_needed('libutils-v32.so')
         .binary_regex_replace(b'_ZN7android6Thread3runEPKcim', b'_ZN7utils326Thread3runEPKcim'),
     (
-        'vendor/lib/libsensorlistener.so',
-        'vendor/lib/libvdis_core.so',
         'vendor/lib64/libsensorlistener.so',
         'vendor/lib64/libvdis_core.so',
     ): blob_fixup()
